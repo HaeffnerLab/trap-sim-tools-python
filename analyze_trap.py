10b0012428d4ccded5be601decda8e1da9dc5d0c
Tokumaru <Tokumaru@users.noreply.github.com>
2014-06-01 22:15:38 -0700

    Manually add analyze_trap

---
+++ analyze_trap.py
@@ -0,0 +1,8 @@
+from all_functions import test_text,import_data,get_trap,expand_field,trap_knobs,set_voltages,post_process_trap
+
+#print(test_text())
+#print(import_data())
+#print(get_trap())
+#print(expand_field())
+#print(trap_knobs())
+#print (post_process_trap())
