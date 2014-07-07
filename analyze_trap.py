411a2c313dea253d9beb7cebda8ea8186f8d4e0c
Tokumaru <Tokumaru@users.noreply.github.com>
2014-07-07 16:24:09 -0700

    Added tutorial abridged comments

--- analyze_trap.py
+++ analyze_trap.py
@@ -1,6 +1,8 @@
-from all_functions import test_text,import_data,get_trap,expand_field,trap_knobs,set_voltages,post_process_trap
+from all_functions import test_text,import_data,get_trap,expand_field,trap_knobs,set_voltages,post_process_trap,test_fourth
+"""Run import_data and get_trap to convert to python data structuires from BEM-solver text files.
+Then run expand_field and trap_knobs to construct the C file for LabRad.
+Finally, run post_process_trap to calculate the trap frequencies."""
 
-#print(test_text())
 #print(import_data())
 #print(get_trap())
 #print(expand_field())
